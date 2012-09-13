16fb81fb03617d6f1ff556f4404e4ec5240cfc5e
Mats Rauhala <mats.rauhala@gmail.com>
2012-09-13 10:31:23 +0300

    Msg parser

--- server.hs
+++ server.hs
@@ -3,9 +3,35 @@
 module Main where
 
 import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as T
+import Control.Monad.Error
+import Control.Monad.Identity (runIdentity)
 
 type Bucket = Text
 type Value = Double
-data Type = Counter | Absolute
-data Aggregate = Average | Max | Min
-data Stats = Stats Bucket Value Type Aggregate
+data Type = Counter | Absolute deriving Show
+data Aggregate = Average | Max | Min | Sum deriving Show
+data Stats = Stats Bucket Value Type Aggregate deriving Show
+
+parseMsg :: Text -> Either String Stats
+parseMsg msg = runIdentity $ runErrorT $ do
+  [bucket, value, _type, aggregate] <- parseMsg msg
+  _type' <- parseType _type
+  value' <- parseValue value
+  aggregate' <- parseAggregate aggregate
+  return $ Stats bucket value' _type' aggregate'
+  where
+    parseMsg msg = case T.splitOn "|" msg of
+                    ret@[_, _, _, _] -> return ret
+                    _ -> fail $ "Could not parse msg: " ++ (T.unpack msg)
+    parseType (T.stripPrefix "counter" -> Just _) = return Counter
+    parseType (T.stripPrefix "absolute" -> Just _) = return Absolute
+    parseType _type = fail $ "Could not parse type: " ++ (T.unpack _type)
+    parseValue value = case reads (T.unpack value) of
+                            [(value', _)] -> return value'
+                            _ -> fail $ "Could not parse value: " ++ (T.unpack value)
+    parseAggregate (T.stripPrefix "average" -> Just _) = return Average
+    parseAggregate (T.stripPrefix "max" -> Just _) = return Max
+    parseAggregate (T.stripPrefix "min" -> Just _) = return Min
+    parseAggregate (T.stripPrefix "sum" -> Just _) = return Sum
+    parseAggregate aggregate = fail $ "Could not parse aggregate: " ++ (T.unpack aggregate)
